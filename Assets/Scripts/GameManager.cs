9a7d9bf9135fe466c14a622279b82cb6e52b8777
Alex Metcalfe <walkingshrub@gmail.com>
2018-04-14 14:26:31 -0400

    Initial upload of old project.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Hv_coreofinal_AudioLib Core1;
+
+
+
+
+
+	public float AmpOut = 1;
+	public float metrotime = 15000;
+	public float distlevel = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		//AmpOut = HeavyScript.
+	
+		transform.localScale += new Vector3(0 * AmpOut, 0 * AmpOut, 0 * AmpOut);
+
+		}
+	}
+

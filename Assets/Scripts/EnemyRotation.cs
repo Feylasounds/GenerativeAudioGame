502f7235805ca958c48340df2851ebbacb3a474e
Alex Metcalfe <38232476+Feylasounds@users.noreply.github.com>
2019-01-19 20:17:54 -0500

    Enemy rotation around cores

---
+++ Assets/Scripts/EnemyRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRotation : MonoBehaviour
+{
+
+    public GameObject mainObject;
+    private float velocity;
+    private float radius;
+    private Vector3 axis;
+
+    void Start()
+    {
+        velocity = Random.Range(1, 10);
+        radius = Random.Range(6, 10);
+        Vector3 basis1 = Random.onUnitSphere;
+        Vector3 basis2 = Random.onUnitSphere;
+        axis = Vector3.Cross(basis1, basis2);
+        transform.position = basis1 * radius;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, axis, (velocity / radius) * Time.deltaTime);
+        transform.Rotate(0, (360 * Time.deltaTime / 4), 0);
+    }
+}
